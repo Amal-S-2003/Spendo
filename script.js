1a96b63214c53b0fdfaff438d7a5ace8c2afb854
Amal-S-2003 <amalshibu379@gmail.com>
2024-10-18 06:26:40 +0530

    register and login from

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const container = document.getElementById('container');
+
+
+function goLogin(){
+container.classList.add("abc");
+
+}
+
+function goReg(){
+  container.classList.remove("abc");
+}
